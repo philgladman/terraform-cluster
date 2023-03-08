e194925184c3124355f61843cd1cad7f37bf2a22
Phillip Gladman <gladman_phillip@bah.com>
2023-03-08 10:53:58 -0500

    add lambda to stop ec2 instances nightly

---
+++ iac/terraform/modules/lambda/files/stop_ec2/stop_ec2.py
@@ -0,0 +1,46 @@
+import boto3
+import logging
+import botocore
+import os
+import jmespath
+
+# set up simple logging
+logging.getLogger().setLevel(os.environ['LOGGING_LEVEL'])
+
+# Set Variables
+region = os.environ.get('REGION')
+
+# Set up connection to AWS EC@
+ec2 = boto3.client('ec2', region_name=region)
+
+def get_instance_ids():
+    response = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'instance-state-name',
+                'Values': ['running']
+            }
+        ]
+    )
+    instance_ids = jmespath.search('Reservations[].Instances[].InstanceId', response)
+
+    return instance_ids
+
+def lambda_handler(event, context):
+    """When triggered, the lambda handler will stop all Running EC2 Instances"""
+    instance_ids = get_instance_ids()
+    if instance_ids:
+        for instance in instance_ids:
+            try:
+                logging.info(" Attempting to stop EC2 Instance: %s\n", instance)
+                response = ec2.stop_instances(InstanceIds=[instance])
+                print(response, "\n")
+            except botocore.exceptions.ClientError as error:
+                logging.error(
+                    " Cannot stop EC2 Instance: %s\n%s", instance,
+                        error.response['Error']['Message']
+                )
+    else:
+        logging.info(" There are no Running EC2 Instances to stop")
+
+lambda_handler("event", "context")
--- iac/terraform/modules/lambda/main.tf
+++ iac/terraform/modules/lambda/main.tf
@@ -5,6 +5,10 @@ locals {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
+################################################################################
+# Lambda to send quarterly Emails
+################################################################################
+
 module "quarterly_email" {
   source = "terraform-aws-modules/lambda/aws"
 
@@ -68,4 +72,74 @@ resource "aws_lambda_permission" "allow_quarterly_cron" {
   function_name = module.quarterly_email.lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.quarterly_cron.arn
+}
+
+################################################################################
+# Lambda to stop all EC2 Instances every night
+################################################################################
+
+module "stop_ec2" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  description   = "Lambda Function to go off every night to Stop All EC2 Instances"
+  function_name = "${local.uname}-stop-ec2"
+  create_role   = true
+  handler       = "stop_ec2.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 180
+
+  source_path = "${path.module}/files/stop_ec2"
+
+  /* environment_variables = {
+    LOGGING_LEVEL = "${var.logging_level}"
+    REGION        = "${var.region}"
+    SNS_TOPIC_ARN = "${var.sns_topic_arn}"
+  } */
+}
+
+# tfsec:ignore:no-policy-wildcards
+resource "aws_iam_policy" "allow_stop_ec2" {
+
+  name        = "${local.uname}-allow-stop-stop-ec2"
+  path        = "/"
+  description = "AWS IAM Policy for lambda to stop EC2 Instances"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ec2:StopInstances",
+            "ec2:DescribeInstances",
+          ],
+          "Resource" : "*"
+        }
+      ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_stop_ec2_role" {
+  role       = module.stop_ec2.lambda_role_name       
+  policy_arn = aws_iam_policy.allow_stop_ec2.arn
+}
+
+resource "aws_cloudwatch_event_rule" "stop_ec2" {
+  name                = "${local.uname}-stop-ec2"
+  description         = "Cronjob to go off every Night at 9:30pm EST"
+  schedule_expression = "cron(30 2 * * ? *)"
+  event_bus_name      = "default"
+}
+
+resource "aws_cloudwatch_event_target" "stop_ec2_target" {
+  arn  = module.stop_ec2.lambda_function_arn
+  rule = aws_cloudwatch_event_rule.stop_ec2.name
+}
+
+resource "aws_lambda_permission" "allow_stop_ec2_cron" {
+  statement_id  = "AllowExecutionFromQuartlyCron"
+  action        = "lambda:InvokeFunction"
+  function_name = module.stop_ec2.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.stop_ec2.arn
 }
